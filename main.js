5d1ba9862a5e1d821b23e2938c2b6031c5264e95
karim <karim-megri@hotmail.fr>
2023-01-11 09:31:11 +0100

    exo8

---
+++ main.js
@@ -0,0 +1,21 @@
+let res = prompt("Veuillez saisir un mot : ");
+
+function palindrome (res) {
+  
+  let resReverser = res.split('').reverse().join('');
+
+  return res === resReverser;
+ 
+}
+
+// if( palindrome) {
+//     alert ( "le mot est un palindrome !");
+// } else{
+//   alert("ce mot n'est pas un palindrome ! ");
+// }
+
+function palindrome(res){
+  if ( true) {
+  alert ( "le mot est un palindrome !");
+  }
+}
